13de89103d7463904be4df81d1643557135e35e1
ngockhuat <ngockhuat57@gmail.com>
2023-03-01 04:57:01 +0700

    first commit

---
+++ lesson_1/main.js
@@ -0,0 +1,16 @@
+// alert('Hi javascript basic!')
+// var fullName = 'khuat thi ngoc';//co the su dung day phay hoac ko
+ //var age = 19;//
+// alert(fullName);
+// alert(age);
+// var fullName = 'khuat thi ngoc';
+// console.error('day la thong bao');
+
+//  //confirm('xac nhan ban du tuoi');
+//  prompt('xac nhan du tuoi')
+//  setTimeout(function() {//cho doan code chay trong 1 khoang thoi gian chayj 1 lan
+//     alert('thong bao')
+//     console.log('day la thong bao' + Math.random);
+//  }, 1000)
+ var a = 1 * 2;
+  alert(a);
